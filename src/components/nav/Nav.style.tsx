2d7d0dd1e2f0ed3a11f133104f3516b21f04abba
sonsu0710 <sonsu0710@gmail.com>
2022-11-18 09:20:57 +0900

    first commit

---
+++ src/components/nav/Nav.style.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Navbar = styled.div`
+  width: 100vw;
+  height: 80px;
+  border-bottom: solid 1px rgba(0,0,0,0.07);
+`
+
+export {Navbar}
---
+++ src/components/nav/Nav.tsx
@@ -0,0 +1,12 @@
+import {Navbar} from "./Nav.style";
+
+// Styled Components
+function Nav() {
+    return (
+        <>
+            <Navbar />
+        </>
+    )
+}
+
+export {Nav}
